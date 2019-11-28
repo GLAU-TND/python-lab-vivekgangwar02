3dfdb6787a58c63e238a8797ee8ca5d0d1883c4e
vivek gangwar <vivekgangwar02@gmail.com>
2019-11-28 16:36:11 +0530

    Q2

--- Q2.py
+++ Q2.py
@@ -3,17 +3,17 @@ def exceptionHandling():
     try:
         a+='string'
     except Exception as e:
-        print(e)
+        print(type(e).__name__,e)
 
     try:
         a.abcd()
     except Exception as e:
-        print(e)
+        print(type(e).__name__,e)
 
     try:
         int('string')
     except Exception as e:
-        print(e)
+        print(type(e).__name__,e)
 
 if __name__=="__main__":
     exceptionHandling()
